02dabd859ff241f080d71969166a81bfdf7a0efc
Anton Babenko <antonwarbeck123@gmail.com>
2023-09-01 13:34:07 +0300

    Added save every opt

--- train.py
+++ train.py
@@ -96,17 +96,18 @@ def train(opt):
             pbar.set_postfix(MSE=loss.item())
             logger.add_scalar("MSE", loss.item(), global_step=epoch * dlength + i)
 
-        sampled_images = diffusion.sample(model, n=imgs.shape[0])
-        img_path = os.path.join("results", f"{epoch}.jpg")
-        save_images(sampled_images, img_path)
-        ema_sampled_images = diffusion.sample(ema_model, n=imgs.shape[0])
-        ema_img_path = os.path.join("results", f"ema_{epoch}.jpg")
-        save_images(ema_sampled_images, ema_img_path)
+        if epoch % opt.save_every == 0:
+            sampled_images = diffusion.sample(model, n=imgs.shape[0])
+            img_path = os.path.join("results", f"{epoch}.jpg")
+            save_images(sampled_images, img_path)
+            ema_sampled_images = diffusion.sample(ema_model, n=imgs.shape[0])
+            ema_img_path = os.path.join("results", f"ema_{epoch}.jpg")
+            save_images(ema_sampled_images, ema_img_path)
 
-        checkpoint_path = os.path.join("models", "ckpt.pt")
-        torch.save(model.state_dict(), checkpoint_path)
-        ema_checkpoint_path = os.path.join("models", "ema_ckpt.pt")
-        torch.save(ema_model.state_dict(), ema_checkpoint_path)
+            checkpoint_path = os.path.join("models", "ckpt.pt")
+            torch.save(model.state_dict(), checkpoint_path)
+            ema_checkpoint_path = os.path.join("models", "ema_ckpt.pt")
+            torch.save(ema_model.state_dict(), ema_checkpoint_path)
 
 
 def get_conf():
@@ -117,6 +118,7 @@ def get_conf():
     args.img_size = 64
     args.device = "cuda"
     args.lr = 2e-4
+    args.save_every = 1
 
     return args
 
